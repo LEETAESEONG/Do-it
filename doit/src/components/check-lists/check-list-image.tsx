18d15c83b6864ac103d34508d98a0a9d7f039426
이태성 <ga1754@naver.com>
2024-07-31 20:46:57 +0900

    [update] 이미지 제거 버튼 추가

--- doit/src/components/check-lists/check-list-image.tsx
+++ doit/src/components/check-lists/check-list-image.tsx
@@ -1,7 +1,7 @@
 import { useRef } from "react";
 import Image from "next/image";
 import smallImg from "./assets/img.png";
-import { PlusIcon } from "@heroicons/react/16/solid";
+import { PlusIcon, XMarkIcon } from "@heroicons/react/16/solid";
 
 type Props = {
   src: string | null;
@@ -31,16 +31,33 @@ export default function CheckListImage({ src, setImageUrl }: Props) {
   return (
     <div className="relative h-311 box-border border-dashed border-2 border-slate-300 rounded-3xl flex justify-center items-center hover:opacity-80 mb-5 desktop:col-start-2 desktop:col-end-6 tablet:w-full phone:w-full phone:mb-4">
       {src ? (
-        <Image src={src} alt="Uploaded Image" className="object-contain" />
+        <>
+          <Image
+            src={src}
+            alt="Uploaded Image"
+            fill={true}
+            className="object-contain"
+          />
+          <div
+            className="absolute bottom-4 right-4 rounded-full border-2 border-slate-900 border-solid size-16 bg-rose-500 flex justify-center items-center cursor-pointer hover:opacity-70"
+            onClick={() => {
+              setImageUrl(null);
+            }}
+          >
+            <XMarkIcon className="size-10 fill-white" />
+          </div>
+        </>
       ) : (
-        <Image src={smallImg} alt="Placeholder Image" />
+        <>
+          <Image src={smallImg} alt="Placeholder Image" />
+          <div
+            className="absolute bottom-4 right-4 rounded-full size-16 bg-slate-200 flex justify-center items-center cursor-pointer hover:opacity-70"
+            onClick={handleClick}
+          >
+            <PlusIcon className="size-10 fill-slate-500" />
+          </div>
+        </>
       )}
-      <div
-        className="absolute bottom-4 right-4 rounded-full size-16 bg-slate-200 flex justify-center items-center cursor-pointer hover:opacity-70"
-        onClick={handleClick}
-      >
-        <PlusIcon className="size-10 fill-slate-500" />
-      </div>
       <input
         type="file"
         ref={fileInputRef}
